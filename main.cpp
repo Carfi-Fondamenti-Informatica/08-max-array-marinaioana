146886f9a5310fd142fbc25c6fa3ee369ac1e8ff
marinaioana <115020895+marinaioana@users.noreply.github.com>
2022-12-11 19:28:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+
+    int a=0;
+     cin>>a;
+     float array[a];
+     for( int i=0; i<a; i++){
+         cin>> array[a];
+     }
+     cout<< numeromaggiore (array, a)<< endl;
+     return 0;
+
 }
+
+
