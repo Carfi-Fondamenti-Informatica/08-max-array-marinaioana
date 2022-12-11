31280623329ac8aa08c55fcf3ffe79776e94303c
marinaioana <115020895+marinaioana@users.noreply.github.com>
2022-12-11 19:25:46 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,9 @@
+float numeromaggiore( float array [], int a){
+    for (int i=0; i < a; i++){
+}     if( array[0] > array [i]){
+
+    }else {
+        array[0]= array [i];
+    }
+return array[0];
 
