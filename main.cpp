b2e67a250464f1a64f544bea02ccb93c880b926a
marinaioana <115020895+marinaioana@users.noreply.github.com>
2022-12-11 19:37:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main() {
 
-    int a=0;
+    int a = 0;
      cin>>a;
-     float array[a];
+     float numeri[a];
      for( int i=0; i<a; i++){
-         cin>> array[a];
+         cin>> numeri[i];
      }
-     cout<< numeromaggiore (array, a)<< endl;
+     cout << numeromaggiore (numeri, a) << endl;
      return 0;
 
 }
